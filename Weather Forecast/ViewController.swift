d50d25c559ca29f027ffa02c8d18de73cec506f0
Abhishek Binniwale <abhishek.binniwale@mavenir.com>
2020-08-29 13:35:39 +0530

    Initial Commit

---
+++ Weather Forecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Forecast
+//
+//  Created by Abhishek Binniwale on 29/08/20.
+//  Copyright © 2020 Abhishek Binniwale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather ForecastTests/Weather_ForecastTests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_ForecastTests.swift
+//  Weather ForecastTests
+//
+//  Created by Abhishek Binniwale on 29/08/20.
+//  Copyright © 2020 Abhishek Binniwale. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Forecast
+
+class Weather_ForecastTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
